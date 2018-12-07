18ec700eae5553ada26cf6a1d9a125b4ccb63925
Cameron Powell <cameronspowell@gmail.com>
2018-12-07 00:39:31 -0500

    Solved day 6 problem 1

---
+++ day6_1.py
@@ -0,0 +1,145 @@
+""" day6_1.py -> This module solves the first problem of the sixth day
+    for the advent of code 2018 """
+
+class Coordinate:
+    """ A coordinate class for getting part or all of a coordinate.
+        also contains helper functions for working with coordinates """
+    def __init__(self, raw_coord):
+        self.x_coord = None
+        self.y_coord = None
+        self.coord = (None, None)
+        self.parse_raw(raw_coord)
+
+    def __str__(self):
+        return '(%s, %s)' % (self.x_coord, self.y_coord)
+
+    def __repr__(self):
+        return '(%s, %s)' % (self.x_coord, self.y_coord)
+
+    def __eq__(self, other):
+        return self.x_coord == other.x_coord and self.y_coord == other.y_coord
+
+    def parse_raw(self, raw_coord):
+        """ Given a raw (string) of a coordinate, parses it and returns
+            the x_dim, y_dim, and (x_dim,y_dim) parts in a tuple """
+        raw_coord = raw_coord.strip()
+        raw_coord = raw_coord.replace(' ', '')
+        raw_coords = raw_coord.split(',')
+        self.x_coord = int(raw_coords[0])
+        self.y_coord = int(raw_coords[1])
+        self.coord = ()
+
+    def manhattan_distance(self, other):
+        """ Calculates the manhattan distance between this coordinate
+            and another coordinate """
+        x_diff = abs(self.x_coord - other.x_coord)
+        y_diff = abs(self.y_coord - other.y_coord)
+        return x_diff + y_diff
+
+class Grid:
+    """ A grid class for containing the grid structure and metadata and
+        methods for working with it """
+    def __init__(self, boundaries_tup, grid):
+        self.smallest_x = boundaries_tup[0]
+        self.smallest_y = boundaries_tup[1]
+        self.largest_x = boundaries_tup[2]
+        self.largest_y = boundaries_tup[3]
+        self.grid = grid
+
+    @classmethod
+    def manhattan_grid(cls, coordinates):
+        """ Generates a grid showing the coordinate at the closest manhattan
+        distance at each point """
+        # Get the shape of the grid
+        sm_x, lg_x, sm_y, lg_y = Grid.grid_shape(coordinates)
+        # Construct the grid
+        grid = {}
+        for x_dim in range(sm_x, lg_x+1):
+            grid[x_dim] = {}
+            for y_dim in range(sm_y, lg_y+1):
+                # Set each value to be the closest coordinate
+                closest = Coordinate('%s,%s' % (x_dim, y_dim))
+                grid[x_dim][y_dim] = closest_manhattan(closest, coordinates)
+        return cls((sm_x, sm_y, lg_x, lg_y), grid)
+
+    @staticmethod
+    def grid_shape(coordinates):
+        """ Gets the smallest and largest values for X and Y in a list of
+            coordinates and returns them in a tuple """
+        # Define initial values based on the first coordinate
+        smallest_x, largest_x = (coordinates[0].x_coord, coordinates[0].x_coord)
+        smallest_y, largest_y = (coordinates[0].y_coord, coordinates[0].y_coord)
+        # Search for the true values
+        for coordinate in coordinates:
+            smallest_x = coordinate.x_coord if coordinate.x_coord < smallest_x else smallest_x
+            largest_x = coordinate.x_coord if coordinate.x_coord > largest_x else largest_x
+            smallest_y = coordinate.y_coord if coordinate.y_coord < smallest_y else smallest_y
+            largest_y = coordinate.y_coord if coordinate.y_coord > largest_y else largest_y
+        # Return the results
+        return (smallest_x, largest_x, smallest_y, largest_y)
+
+    def get_finite_coordinates(self, coordinates):
+        """ Creates a list of coordinates which are contained in a finite area
+            within the grid """
+            # Calculate all coordinates which are infinite (not on an edge)
+        bound_coordinates = [coordinate for coordinate in coordinates if\
+            coordinate.x_coord != self.smallest_x and\
+            coordinate.x_coord != self.largest_x and\
+            coordinate.y_coord != self.smallest_y and\
+            coordinate.y_coord != self.largest_y]
+        return bound_coordinates
+
+    def coordinate_area(self, coordinate):
+        """ Calculates the area a given coordinate holds in a given grid """
+        area = 0
+        # Count each space in the grid which this coordinate holds
+        for x_dim in range(self.smallest_x, self.largest_x+1):
+            for y_dim in range(self.smallest_y, self.largest_y+1):
+                if self.grid[x_dim][y_dim] == coordinate:
+                    area += 1
+        return area
+
+    def largest_bound_area(self, coordinates):
+        """ Determines which coordinate in a given list encapsulates the
+            largest area of a given grid and returns that area """
+        largest_area = 0
+        # Check the area of each coordinate given
+        for coordinate in coordinates:
+            coord_area = self.coordinate_area(coordinate)
+            # Update the largest area if a bigger one is found
+            if coord_area > largest_area:
+                largest_area = coord_area
+        return largest_area
+
+
+def closest_manhattan(to_coord, coordinates):
+    """ Calculates which coordinate in a given list of coordinates is closest
+        to the coordinate passed in """
+    # Set the initial values to compare against using the first value in coordinates
+    closest_coordinate = coordinates[0]
+    closest_coordinate_dist = closest_coordinate.manhattan_distance(to_coord)
+    tied = False
+    # Search for the closest coordinate
+    for coord in coordinates[1:]:
+        # Get the manhattan distance between these coordinates
+        dist_between = coord.manhattan_distance(to_coord)
+        # Update the newest closest coordinate if current coord is closer
+        if dist_between < closest_coordinate_dist:
+            tied = False
+            closest_coordinate = coord
+            closest_coordinate_dist = dist_between
+        elif dist_between == closest_coordinate_dist:
+            tied = True
+    return Coordinate('-1,-1') if tied else closest_coordinate
+
+
+if __name__ == '__main__':
+    # Read in the coordinates
+    with open('day6.txt') as coordinates_file:
+        COORDINATES = [Coordinate(coord_string) for coord_string in coordinates_file]
+    # Create a grid showing which coordinate is closest to each point
+    GRID = Grid.manhattan_grid(COORDINATES)
+    # Create a list of finite-area coordinates
+    FINITE_COORDINATES = GRID.get_finite_coordinates(COORDINATES)
+    # Determine which finitely bound coordinate encapsulates the largest area
+    print(str(GRID.largest_bound_area(FINITE_COORDINATES)))
